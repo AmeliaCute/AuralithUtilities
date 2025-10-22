3eadbae2bfa7dbfef4a36c96398b0434a63f4242
AmeliaCute <48628282+AmeliaCute@users.noreply.github.com>
2025-10-22 16:59:30 +0100

    adding lot of shit

--- src/main/java/gg/amecute/auralithutilities/Mystical/ModCrops.java
+++ src/main/java/gg/amecute/auralithutilities/Mystical/ModCrops.java
@@ -16,13 +16,36 @@ public final class ModCrops
     public static final Crop ANTIMONY =
             new Crop(AuralithUtilities.resGet("antimony"), CropTier.FOUR, CropType.RESOURCE, LazyIngredient.item("modern_industrialization:antimony_ingot"));
 
+    public static final Crop CHROMIUM_HOT =
+            new Crop(AuralithUtilities.resGet("chromium_hot"), ModCropTiers.SIX, CropType.RESOURCE, LazyIngredient.item("modern_industrialization:chromium_hot_ingot"));
+
+    public static final Crop KANTHAL_HOT =
+            new Crop(AuralithUtilities.resGet("kanthal_hot"), ModCropTiers.SIX, CropType.RESOURCE, LazyIngredient.item("modern_industrialization:kanthal_hot_ingot"));
+
+    public static final Crop SUPERCONDUCTOR_HOT =
+            new Crop(AuralithUtilities.resGet("superconductor_hot"), ModCropTiers.SIX, CropType.RESOURCE, LazyIngredient.item("modern_industrialization:superconductor_hot_ingot"));
+
     public static final Crop TITANIUM_HOT =
             new Crop(AuralithUtilities.resGet("titanium_hot"), ModCropTiers.SIX, CropType.RESOURCE, LazyIngredient.item("modern_industrialization:titanium_hot_ingot"));
+
+    public static final Crop TUNGSTEN =
+            new Crop(AuralithUtilities.resGet("tungsten"), ModCropTiers.SIX, CropType.RESOURCE, LazyIngredient.item("modern_industrialization:tungsten_ingot"));
+
+    public static final Crop MANGANESE_DUST =
+            new Crop(AuralithUtilities.resGet("manganese_dust"), CropTier.FOUR, CropType.RESOURCE, LazyIngredient.item("modern_industrialization:manganese_dust"));
 
     public static void onRegisterCrops(ICropRegistry registry)
     {
         registry.register(withRequiredMods(ANTIMONY, "modern_industrialization"));
+        registry.register(withRequiredMods(TUNGSTEN, "modern_industrialization"));
+
+        registry.register(withRequiredMods(CHROMIUM_HOT, "modern_industrialization"));
+        registry.register(withRequiredMods(KANTHAL_HOT, "modern_industrialization"));
+        registry.register(withRequiredMods(SUPERCONDUCTOR_HOT, "modern_industrialization"));
         registry.register(withRequiredMods(TITANIUM_HOT, "modern_industrialization"));
+
+        registry.register(withRequiredMods(MANGANESE_DUST, "modern_industrialization"));
+
     }
 
     private static Crop withRequiredMods(Crop crop, String... mods)
