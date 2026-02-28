b22ee6b77eee6fa7fc5f6636e2e5251852e95fe9
AmeliaCute <48628282+AmeliaCute@users.noreply.github.com>
2026-02-28 22:03:27 +0000

    Making the mortar better, kinda

--- src/main/java/gg/amecute/auralithutilities/Registries/AuralithItems.java
+++ src/main/java/gg/amecute/auralithutilities/Registries/AuralithItems.java
@@ -1,13 +1,39 @@
 package gg.amecute.auralithutilities.Registries;
 
 import gg.amecute.auralithutilities.AuralithUtilities;
+import gg.amecute.auralithutilities.Item.PestleItem;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
-public class AuralithItems
-{
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Registries.ITEM, AuralithUtilities.MODID);
+public final class AuralithItems {
 
+    public static final DeferredRegister<Item> ITEMS =
+        DeferredRegister.create(Registries.ITEM, AuralithUtilities.MODID);
 
+    public static final DeferredHolder<Item, PestleItem> PESTLE =
+        ITEMS.register("pestle", PestleItem::new);
+
+    public static final DeferredHolder<Item, Item> CRUSHED_ANTIMONY =
+        registerSimple("crushed_antimony");
+
+    public static final DeferredHolder<Item, Item> CRUSHED_IRIDIUM =
+        registerSimple("crushed_iridium");
+
+    public static final DeferredHolder<Item, Item> CRUSHED_SILVER =
+        registerSimple("crushed_silver");
+
+    public static final DeferredHolder<Item, Item> CRUSHED_TITANIUM =
+        registerSimple("crushed_titanium");
+
+    public static final DeferredHolder<Item, Item> CRUSHED_TUNGSTEN =
+        registerSimple("crushed_tungsten");
+
+    private static DeferredHolder<Item, Item> registerSimple(String name) 
+    {
+    return ITEMS.register(name, () -> new Item(new Item.Properties()));
+    }
+
+    private AuralithItems() {}
 }
